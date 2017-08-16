20006e78ca4b6428b30a03fe14368cbe775c8c3f
jiledar bind <bind060792@gmail.com>
2017-08-16 13:46:18 +0530

    Change-Id: I9f01f641f5413a81a1e36bd0038757411a0c44b0

---
+++ src/test/java/com/tests/TestCaseOne.java
@@ -0,0 +1,11 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+public class TestCaseOne {
+	@Test
+	public void testOne(){
+		System.out.println("This is tescase one passed");
+	}
+
+}
